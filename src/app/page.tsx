c6a40977f667353d289fb0581559662e1c16f5d5
Gayun00 <gayunlee11@gmail.com>
2023-10-03 13:29:59 +0900

    Add: show article list with fetched data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,7 @@
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { useForm } from "react-hook-form";
 import {
   Form,
@@ -22,9 +15,18 @@ import {
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Badge } from "@/components/ui/badge";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import Image from "next/image";
+
+interface Article {
+  title: string;
+  text: string;
+  thumbnail: string;
+  keywords: string[];
+}
 
 export default function Home() {
+  const [articles, setArticles] = useState<Article[]>([]);
   const formSchema = z.object({
     url: z.string().refine(
       (value) => {
@@ -65,7 +67,7 @@ export default function Home() {
       },
     })
       .then((res) => res.json())
-      .then((data) => console.log("data", data));
+      .then((data) => setArticles(data.articles));
   };
 
   useEffect(() => {
@@ -80,72 +82,54 @@ export default function Home() {
       <Card>
         <CardHeader>
           <CardTitle>Enter url</CardTitle>
-          <CardDescription>
-            <Form {...form}>
-              <form
-                onSubmit={form.handleSubmit(onSubmit)}
-                className="flex space-y-8">
-                <FormField
-                  control={form.control}
-                  name="url"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>url</FormLabel>
-                      <FormControl>
-                        <Input placeholder="shadcn" {...field} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <Button type="submit">Submit</Button>
-              </form>
-            </Form>
-          </CardDescription>
         </CardHeader>
         <CardContent>
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="flex space-y-8">
+              <FormField
+                control={form.control}
+                name="url"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>url</FormLabel>
+                    <FormControl>
+                      <Input placeholder="shadcn" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button type="submit">Submit</Button>
+            </form>
+          </Form>
           <div className="mb-4">
             <Badge>Next.js</Badge>
           </div>
           <CardContent className="grid gap-4 px-0">
-            <div className="flex items-center space-x-4 rounded-md border p-4 m-0">
-              <div>img</div>
-              <div className="flex-1 space-y-1">
-                <p className="text-sm font-medium leading-none">
-                  Push Notifications
-                </p>
-                <p className="text-sm text-muted-foreground">
-                  Send notifications to device.
-                </p>
+            {articles.map((article) => (
+              <div
+                key={article.title}
+                className="flex items-center space-x-4 rounded-md border p-4 m-0">
+                <Image
+                  src={article.thumbnail}
+                  width={20}
+                  height={20}
+                  alt="thumbnail"
+                />
+                <div className="flex-1 space-y-1 w-1/3">
+                  <p className="text-sm font-medium leading-none text-ellipsis overflow-hidden whitespace-nowrap">
+                    {article.title}
+                  </p>
+                  <p className="text-sm text-muted-foreground text-ellipsis overflow-hidden whitespace-nowrap">
+                    {article.text.slice(0, 30)}
+                  </p>
+                </div>
               </div>
-            </div>
-
-            <div className="flex items-center space-x-4 rounded-md border p-4 m-0">
-              <div>img</div>
-              <div className="flex-1 space-y-1">
-                <p className="text-sm font-medium leading-none">
-                  Push Notifications
-                </p>
-                <p className="text-sm text-muted-foreground">
-                  Send notifications to device.
-                </p>
-              </div>
-            </div>
-
-            <div className="flex items-center space-x-4 rounded-md border p-4 m-0">
-              <div>img</div>
-              <div className="flex-1 space-y-1">
-                <p className="text-sm font-medium leading-none">
-                  Push Notifications
-                </p>
-                <p className="text-sm text-muted-foreground">
-                  Send notifications to device.
-                </p>
-              </div>
-            </div>
+            ))}
           </CardContent>
         </CardContent>
-        <CardFooter>techorage</CardFooter>
       </Card>
     </main>
   );
