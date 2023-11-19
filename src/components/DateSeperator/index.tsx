ab6521fd319f450b183a74921d7c1a50ef028a7b
Gayun00 <gayunlee11@gmail.com>
2023-11-19 14:30:20 +0900

    Modify: date seperator

--- src/components/DateSeperator/index.tsx
+++ src/components/DateSeperator/index.tsx
@@ -7,7 +7,7 @@ interface Props {
 function DateSeperator({ date }: Props) {
   const articleDate = new Date(date).toDateString();
 
-  return <p className="my-4 text-center text-sm">{articleDate}</p>;
+  return <p className="mt-4 text-center text-xs">{articleDate}</p>;
 }
 
 export default DateSeperator;
